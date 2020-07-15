cd6d438d0eb28bb8cad2d8eb64108bf57aa753b1
Daniel W <danieldwalford@gmail.com>
2020-07-15 14:25:27 +0100

    Initial project commit

---
+++ js/app.js
@@ -0,0 +1,346 @@
+// API url
+const url = [
+    './data/',
+    '.tsv'
+];
+
+// DOM elements
+const spinner = document.getElementById('loader');
+const selector = document.getElementById('selector');
+const legend = document.getElementById('legend');
+const units = document.getElementById('units');
+const count = document.getElementById('count');
+
+// Colors and options
+const colors = [
+    'rgb(43, 175, 219)',
+    'rgb(234, 56, 179)',
+    'rgb(43, 225, 179)',
+    'rgb(232, 241, 47)',
+    'rgb(247, 93, 43)'
+];
+
+const options = {
+    'Ethnicity': 'ethnicity',
+    'Social grade': 'class',
+    'Hours worked': 'hours',
+    'Housing type': 'home',
+    'Housing tenure': 'tenure'
+};
+
+const unitise = {
+    'ethnicity': 'people',
+    'class': 'people',
+    'hours': 'workers',
+    'home': 'homes',
+    'tenure': 'homes'
+};
+
+// Set null variables
+var data = {
+    'headers': [],
+    'values': {},
+    'totals': [],
+    'perc': [],
+};
+var store = {};
+
+// Create popup class for map tooltips
+var popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+});
+
+//https://api.mapbox.com/v4/mapbox.enterprise-boundaries-a2-v2/tilequery/12.87,43.100.json?access_token=pk.eyJ1IjoiZGFuZHdhbCIsImEiOiJja2JodjE3bnUwOTNvMnNwdmVpdWU2cXoxIn0.Zwhbvr7YGa2TAE4iNpV6aA
+
+// Function to turn CSV (string) into array of objects
+function tsv2json(string) {
+    let json = {
+        'headers': [],
+        'values': {},
+        'totals': [],
+        'perc': [],
+    };
+    string = string.replace(/['"]+/g, '');
+    let array = string.split('\n');
+    let headers = array[0].split('\t');
+    headers.shift();
+    json.headers = headers;
+    for (i in headers) {
+        json.totals.push(0);
+    }
+    for (var i = 1; i < array.length; i++) {
+        let row = array[i].split('\t');
+        if (row[1]) {
+            let tot = 0;
+            let counts = [];
+            let breaks = [];
+            for (j = 1; j < row.length; j++) {
+                let val = parseInt(row[j]);
+                tot += Math.round(val / 10);
+                counts.push(val);
+                breaks.push(tot);
+                json.totals[j - 1] += val;
+            }
+            json.values[row[0]] = {
+                'counts': counts,
+                'breaks': breaks
+            }
+        }
+    }
+    let sum = 0;
+    for (tot in json.totals) {
+        sum += json.totals[tot];
+    }
+    for (tot in json.totals) {
+        let perc = Math.round(100 * (json.totals[tot] / sum));
+        json.perc.push(perc);
+    }
+    return json;
+}
+
+// Function to get data
+function getData(dim) {
+    spinner.style.display = 'flex';
+    let dataurl = url[0] + dim + url[1];
+    if (!store[dim]) {
+        fetch(dataurl)
+            .then((response) => {
+                return response.text();
+            })
+            .then((tsvdata) => {
+                return tsv2json(tsvdata);
+            })
+            .then((newdata) => {
+                data = newdata;
+                store[dim] = newdata;
+                genLegend(data);
+                clearDots();
+                updateDots();
+                units.innerHTML = unitise[dim];
+                spinner.style.display = 'none';
+                return true;
+            });
+    } else {
+        data = store[dim];
+        genLegend(data);
+        clearDots();
+        updateDots();
+        units.innerHTML = unitise[dim];
+        spinner.style.display = 'none';
+    }
+}
+
+// Function to get color for a value based on breaks
+function getColor(value, breaks) {
+    for (i in breaks) {
+        if (value < breaks[i]) {
+            if (document.getElementById('legend' + i).checked) {
+                return [colors[i], i];
+            } else {
+                return [null, i];
+            }
+        }
+    }
+    return [null, null];
+}
+
+// Function to add interaction to map
+function addInteraction() {
+
+
+    map.on('click', 'local-authority-districts-de-afhpgd', (e) => {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        console.log("coordinates");
+        console.log(coordinates);
+    })
+    // Variable for highlighting areas
+    let hoveredId = null;
+
+    // Show data on hover
+    map.on('mousemove', 'local-authority-districts-de-afhpgd', (e) => {
+        console.log('mouse move and features');
+        if (e.features.length > 0) {
+            if (hoveredId) {
+                map.setFeatureState(
+                    {source: 'composite', sourceLayer: 'Local_Authority_Districts__De-afhpgd', id: hoveredId},
+                    {hover: false}
+                );
+            }
+            hoveredId = e.features[0].id;
+            areaName = e.features[0].properties.lad15nm;
+            map.setFeatureState(
+                {source: 'composite', sourceLayer: 'Local_Authority_Districts__De-afhpgd', id: hoveredId},
+                {hover: true}
+            );
+
+            let text = '<strong>Local Authority: ' + areaName + '</strong>';
+            for (i in data.headers) {
+                text += '<br><span class="dot mr-1" style="background-color:' + colors[i] + ';"></span>' + data.headers[i] + ': ' + data.values[hoveredId].counts[i];
+            }
+
+            // Populate the popup and set its coordinates
+            // based on the feature found.
+            popup
+                .setLngLat(e.lngLat)
+                .setHTML(text)
+                .addTo(map);
+        }
+    });
+
+    // Remove tooltips on mouseleave
+    map.on('mouseleave', 'local-authority-districts-de-afhpgd', () => {
+        if (hoveredId) {
+            map.setFeatureState(
+                {source: 'composite', sourceLayer: 'Local_Authority_Districts__De-afhpgd', id: hoveredId},
+                {hover: false}
+            );
+        }
+        hoveredId = null;
+
+        popup.remove();
+    });
+
+    // Update legend on zoom
+    map.on('zoom', function () {
+        //updateUnits();
+    });
+}
+
+// Function to set properties of map features
+function setProperties(dots) {
+    for (dot in dots) {
+        let code = dots[dot].substring(0, 9);
+        let num = parseInt(dots[dot].substring(9, 11));
+        let color = getColor(num, data.values[code].breaks);
+
+        map.setFeatureState({
+            source: 'dots',
+            sourceLayer: 'dots',
+            id: dots[dot]
+        }, {
+            color: color[0],
+            group: color[1]
+        });
+    }
+    updateLegend();
+}
+
+// Function to check if new dots have been loaded
+function updateDots() {
+    if (data.totals[0]) {
+        let features = map.querySourceFeatures('dots', {'sourceLayer': 'dots'});
+        let newdots = [];
+        for (feature in features) {
+            let id = features[feature].properties.id;
+            let state = map.getFeatureState({
+                source: 'dots',
+                sourceLayer: 'dots',
+                id: id
+            });
+            if (!state['color']) {
+                newdots.push(id);
+            }
+        }
+        setProperties(newdots);
+    }
+}
+
+// Function to update legend
+function updateLegend() {
+
+    // Initialise counts for each group
+    let counts = [];
+    for (i in data.headers) {
+        counts.push(0);
+    }
+
+    // Add add group counts for each visible feature
+    let features = map.queryRenderedFeatures({layers: ['bounds']});
+    let ids = [];
+    for (feature in features) {
+        ids.push(features[feature].id);
+    }
+    ids = ids.filter((v, i, a) => a.indexOf(v) === i);
+    for (i in ids) {
+        let values = data.values[ids[i]].counts;
+        for (val in values) {
+            counts[val] += values[val];
+        }
+    }
+
+    // Turn counts into percentages + render to DOM
+    let sum = counts.reduce((a, b) => a + b);
+    let perc = counts.map((num) => Math.round((num / sum) * 100));
+    for (i in perc) {
+        document.getElementById('perc' + i).innerHTML = perc[i] + '%';
+    }
+}
+
+// Function to generate options + set event listener
+function genOptions(options) {
+    let keys = Object.keys(options);
+    let values = Object.values(options);
+    let html = ""
+    for (i in keys) {
+        let selected = i == 0 ? ' selected="selected"' : "";
+        let option = '<option value="' + values[i] + '"' + selected + '>' + keys[i] + '</option>';
+        html += option;
+    }
+    selector.innerHTML = html;
+    selector.onchange = () => {
+        getData(selector.value);
+    }
+}
+
+// Function to clear map dots styling
+function clearDots() {
+    map.removeFeatureState({
+        source: 'dots',
+        sourceLayer: 'dots'
+    });
+}
+
+// Function to add legend scale
+function genLegend(data) {
+    let html = '';
+    for (i in data.headers) {
+        html += '<p class="mb-1"><span class="dot mr-1" style="background-color:' + colors[i] + ';"></span><input type="checkbox" id="legend' + i + '" checked /> <small>' + data.headers[i] + ' <span id="perc' + i + '"></span> <span class="text-secondary">(' + data.perc[i] + '%)</span></small></p>';
+    }
+    legend.innerHTML = html;
+}
+
+// Function to display units based on zoom
+function updateUnits() {
+    let zoom = map.getZoom();
+    let unit = zoom >= 13 ? 10 : zoom >= 12 ? 20 : zoom >= 11 ? 40 : zoom >= 10 ? 80 : zoom >= 9 ? 160 : 320;
+    count.innerHTML = unit;
+}
+
+// INITIALISE MAP
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuZHdhbCIsImEiOiJja2JodjE3bnUwOTNvMnNwdmVpdWU2cXoxIn0.Zwhbvr7YGa2TAE4iNpV6aA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: './data/style.json',
+    center: [-1.2471735, 50.8625412],
+    zoom: 12,
+    maxZoom: 22,
+    minZoom: 7
+});
+
+// ADD LAYERS + DATA ONCE MAP IS INITIALISED
+map.on('load', function () {
+    //genOptions(options);
+    // makeLayers();
+    // updateUnits();
+    //getData(selector.value);
+    addInteraction();
+    spinner.style.display = 'none';
+});
+
+// Set up an event listener on the map.
+map.on('sourcedata', function (e) {
+    if (map.areTilesLoaded()) {
+        //updateDots();
+    }
+});
